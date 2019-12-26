614c2ea3551062a29eedda8a51b29ce4e0a0d9da
Chris McIntosh <chocrates@github.com>
2019-12-26 16:20:28 -0600

    Refactoring into a class

--- index.js
+++ index.js
@@ -1,85 +1,89 @@
 const winston = require('winston')
-const logger = new winston.createLogger({
-    level: 'info',
-    format: winston.format.json(),
-    transports: [ new winston.transports.Console()]
-})
+class octomock {
+    constructor(){
+        this.logger = new winston.createLogger({
+            level: 'info',
+            format: winston.format.json(),
+            transports: [ new winston.transports.Console()]
+        })
 
-const defaultContext = {
-    payload: {
-        issue: {
-            body: "",
-            user: {}
-        }
-    }
-}
-
-global.mockFunctions = {
-    createInvitation: jest.fn(),
-    getContents: jest.fn(),
-    setFailed: jest.fn((message) => { logger.error(`MOCK ERROR: ${message}`)}),
-    debug: jest.fn(message => {logger.info(`MOCK DEBUG: ${message}`)}),
-    info: jest.fn(message => {logger.info(`MOCK INFO: ${message}`)}),
-    setOutput: jest.fn()
-}
-
-let mockGitHubImplementation = {
-    context: {},
-    GitHub: class {
-        constructor() {
-            this.orgs = {
-                createInvitation: mockFunctions.createInvitation 
-            }
-            this.repos = {
-                getContents: mockFunctions.getContents
+        this.defaultContext = {
+            payload: {
+                issue: {
+                    body: "",
+                    user: {}
+                }
             }
         }
+
+        this.mockFunctions = {
+            createInvitation: jest.fn(),
+            getContents: jest.fn(),
+            setFailed: jest.fn((message) => { logger.error(`MOCK ERROR: ${message}`)}),
+            debug: jest.fn(message => {logger.info(`MOCK DEBUG: ${message}`)}),
+            info: jest.fn(message => {logger.info(`MOCK INFO: ${message}`)}),
+            setOutput: jest.fn()
+        }
+
+        this.mockGitHubImplementation = {
+            context: {},
+            GitHub: class {
+                constructor() {
+                    this.orgs = {
+                        createInvitation: mockFunctions.createInvitation 
+                    }
+                    this.repos = {
+                        getContents: mockFunctions.getContents
+                    }
+                }
+            }
+        }
+
+        this.mockCoreImplementation = {
+            debug: mockFunctions.debug,
+            info: mockFunctions.info,
+            setFailed: mockFunctions.setFailed,
+            setOutput: mockFunctions.setOutput
+        }
+    }
+
+    updateGitHubImplementation(implementation){
+        this.mockGitHubImplementation = implementation
+    }
+
+    getGitHubImplementation(){
+        return this.mockGitHubImplementation
+    }
+
+    getCoreImplementation(){
+        return this.mockCoreImplementation
+    }
+
+    updateCoreImplementation(implementation){
+        this.mockCoreImplementation = implementation
+    }
+
+    resetMocks(){
+        for(let func in this.mockFunctions) {
+            this.mockFunctions[func].mockClear()
+        }
+    }
+
+    updateContext(context){
+        for(let property in context){
+            this.mockGitHubImplementation.context[property] = context[property]
+        } 
+    }
+
+    getContext(){
+        return this.mockGitHubImplementation.context
+    }
+    
+    setup(){
+        jest.mock('@actions/github', () => { return this.mockGitHubImplementation }, {virtual: true})
+        jest.mock('@actions/core', () => { return  this.mockCoreImplementation }, {virtual: true})
+        updateContext(defaultContext)
     }
 }
 
-let mockCoreImplementation = {
-    debug: mockFunctions.debug,
-    info: mockFunctions.info,
-    setFailed: mockFunctions.setFailed,
-    setOutput: mockFunctions.setOutput
-}
-
-global.updateGitHubImplementation = (implementation) => {
-        mockGitHubImplementation = implementation
-}
-
-global.getGitHubImplementation = () => {
-    return mockGitHubImplementation
-}
-
-global.getCoreImplementation = () => {
-    return mockCoreImplementation
-}
-
-global.updateCoreImplementation = (implementation) => {
-        mockCoreImplementation = implementation
-}
-
-global.resetMocks = () => {
-    for(let func in mockFunctions) {
-        mockFunctions[func].mockClear()
-    }
-}
-
-global.updateContext = (context) => {
-    for(let property in context){
-        mockGitHubImplementation.context[property] = context[property]
-    } 
-}
-
-global.getContext = () => {
-    return mockGitHubImplementation.context
-}
-
-const setup = () => {
-    jest.mock('@actions/github', () => { return mockGitHubImplementation }, {virtual: true})
-    jest.mock('@actions/core', () => { return  mockCoreImplementation }, {virtual: true})
-    updateContext(defaultContext)
-}
-
-module.exports = { setup }
+module.exports = { octomock }
